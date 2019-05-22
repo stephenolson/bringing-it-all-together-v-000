0c3105ec7d3ce379a02854f660f9c356b4d46d25
Stephen Olson <stephenolson@gmail.com>
2019-05-22 01:47:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,42 @@
+class Dog
+  
+  attr_accessor :id, :name, :breed
+  @@all = []
+
+
+  def initialize(id:, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+    @@all << self
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db(student)
+    new_student = self.new(student[0], student[1], student[2])
+    new_student
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  
+end
